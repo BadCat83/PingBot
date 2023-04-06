9a8ccf165287ee8f4820b15eb6c683e1447362c4
BadCat83 <30713558+BadCat83@users.noreply.github.com>
2023-04-06 23:33:19 +1000

    Update config.py

--- Config/config.py
+++ Config/config.py
@@ -23,22 +23,3 @@ class Resource(BaseModel):
     resource_name: str
     domain_name: str
     ip_address: IPv4Address
-
-
-if __name__ == '__main__':
-    """allowed_users = {
-        952969226: {'nick': 'bad_cat11', 'user_name': 'Nekhoroshev Konstantin'},
-    }
-    resource_list = {
-        'Mikrotik': '92.119.216.1',
-        'Bitrix': '92.119.216.3',
-        'Checkpoint': '92.119.216.10',
-        'Cisco_Gate': '92.119.216.15',
-    }"""
-    external_data = {
-        'id': 952969226,
-        'nick': 'bad_cat11',
-        'user_name': 'Nekhoroshev Konstantin',
-    }
-    user = User(**external_data)
-    print(user)
