ed1f26a091f8f9771d6b5efc364d3c28239df38c
Константин Нехорошев <nekhoroshevkg@russiancrab.ru>
2023-04-29 17:31:09 +1000

    Added redis storage for online updating the resources for pinging

--- init_bot.py
+++ init_bot.py
@@ -1,9 +1,12 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from redis import Redis
+
 from Config.config import Settings
 import DB.sqlite as db
 
 storage = MemoryStorage()
+resources_storage = Redis(host='localhost', port=6379, decode_responses=True)
 config = Settings()
 
 bot = Bot(token=config.bot_token.get_secret_value(), parse_mode="HTML")
