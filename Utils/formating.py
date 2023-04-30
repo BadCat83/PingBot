421b93e8e75ceed5593cee219fe689b484e901ed
Константин Нехорошев <nekhoroshevkg@russiancrab.ru>
2023-04-30 12:43:25 +1000

    Added some description

--- Utils/formating.py
+++ Utils/formating.py
@@ -1,4 +1,9 @@
+"""Some formatting functions"""
+from datetime import timedelta
+
+
 def format_ending(num: int) -> str:
+    """Formatting ending of the word 'секунда'"""
     sec = num % 10
     if sec == 1:
         return 'у'
@@ -9,13 +14,15 @@ def format_ending(num: int) -> str:
 
 
 def format_resources_list(resources_list: list) -> str:
+    """Formatting string output"""
     formatted_string = ' - '
     for resource in resources_list:
         formatted_string += f'<em><b>{resource[1]}</b></em>' + ', '
     return formatted_string[:-2]
 
 
-def time_format(delta):
+def time_format(delta: timedelta) -> tuple:
+    """Formatting time output"""
     days, seconds = delta.days, delta.seconds
     hours = days * 24 + seconds // 3600
     minutes = (seconds % 3600) // 60
