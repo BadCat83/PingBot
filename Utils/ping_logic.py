2f54d7db4d7977c2a2669023645ec923b0069333
Константин Нехорошев <nekhoroshevkg@russiancrab.ru>
2023-04-30 12:49:09 +1000

    Added some describes. Changed the logic to have the ability to ping the resource that have been added when after the bot was started

--- Utils/ping_logic.py
+++ Utils/ping_logic.py
@@ -7,8 +7,12 @@ from Config.config import TIMEOUT, PING_TIMEOUT
 from Utils.formating import time_format
 from init_bot import db, dp, resources_storage
 
+"""This part of code launches as a separate flow and uses redis as a storage for keeping some variables that needed
+at some logic. I'm new in Redis so I used it to the extent that I understood its work"""
+
 
 async def send_message_to_users(resource_ip: str, text: str) -> None:
+    """This function just sends a message to the user, informing them about the resource status"""
     users = await db.get_resource_users(resource_ip)
     res_name = await db.get_resource_by_ip(resource_ip)
     for user in users:
@@ -16,16 +20,29 @@ async def send_message_to_users(resource_ip: str, text: str) -> None:
 
 
 async def do_ping() -> None:
+    """The main logic of the ping function. Pings all the resources that is read from the database.
+     Then, if one of them has become unavailable, sends the warning message to the subscribed users."""
+    # Waiting while the database is initialized
     await asyncio.sleep(TIMEOUT)
     resources = await db.get_resources_ip()
+    # I know that in this case the Redis losts its advantages, but in this logic it's not necessary
+    # I just wanted to try this technology and have some practice in it
     resources_storage.flushdb()
+    # Adding all the resources in Redis database as LIST
     _ = [resources_storage.lpush('res', resource[0]) for resource in resources]
-    res_list = {resource: {'avail': 1, 'dt': ' ', 'cnt': 0} for resource in resources_storage.lrange('res', 0, -1)}
-    for key, val in res_list.items():
+    # Adding some parameters to keep track of the state of the resource
+    for key, val in {resource: {
+            'avail': 1,
+            'dt': ' ',
+            'cnt': 0
+        } for resource in resources_storage.lrange('res', 0, -1)
+    }.items():
         resources_storage.hmset(key, val)
+    # Time tag to keep track when the resource became to be unavailable
     dt = None
     while True:
-        for ip in res_list:
+        # Getting resource list from Redis to have the ability to add resources in online mode
+        for ip in resources_storage.lrange('res', 0, -1):
             if (resources_storage.hget(ip, 'dt') != ' ') and not dt:
                 dt = datetime.fromtimestamp(int(resources_storage.hget(ip, 'dt')))
             if resources_storage.hget(ip, 'avail') != '0':
@@ -35,13 +52,18 @@ async def do_ping() -> None:
                     # print(f'{ip}:{delay}')
                 except TimeoutError:
                     resources_storage.hset(ip, 'cnt', int(resources_storage.hget(ip, 'cnt')) + 1)
-                    if int(resources_storage.hget(ip, 'cnt')) > 1:
+                    # If there was little packet loss, don't spam the users with the messages
+                    if int(resources_storage.hget(ip, 'cnt')) > 2:
                         resources_storage.hset(ip, 'dt', int(datetime.now().timestamp()))
                         resources_storage.hset(ip, 'avail', 0)
                         await send_message_to_users(ip, 'недоступен')
-            elif (datetime.now() - datetime.fromtimestamp(int(resources_storage.hget(ip, 'dt')))) > timedelta(minutes=1):
+            # To don't spam users with the messages, if resource is unavailable, waiting for 15 minutes and
+            # sending the message again
+            elif (datetime.now() - datetime.fromtimestamp(int(resources_storage.hget(ip, 'dt')))) > timedelta(
+                    minutes=15):
                 await send_message_to_users(ip, 'все еще недоступен')
                 resources_storage.hset(ip, 'dt', int(datetime.now().timestamp()))
+            # If the resource becomes available, sending an appropriate message and turns into normal ping mode
             else:
                 try:
                     await aioping.ping(ip) * 1000
